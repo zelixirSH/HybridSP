8ba0e06f91ab01db68480ae8de2f54685fa30b00
physwzh <129308553+physwzh@users.noreply.github.com>
2026-01-05 14:30:07 +0800

    Update korp_pl.py

--- scripts/korp_pl.py
+++ scripts/korp_pl.py
@@ -13,12 +13,3 @@ def korp_score(korp, pro_pdb, lig_mol2):
     except subprocess.CalledProcessError as e:
         print(f"KORP-PL Error: {e}")
         return None
-
-
-if __name__ == '__main__':
-    korp = '/data_test/home/lzzheng/wzh/softwares/KORP-PL'
-    pro_pdb = '/data_test/home/lzzheng/wzh/softwares/statistical/integrated_model/examples/1a30_protein.pdb'
-    lig_mol2 = '/data_test/home/lzzheng/wzh/softwares/statistical/integrated_model/examples/1a30_decoys.mol2'
-    out = korp_score(korp, pro_pdb, lig_mol2)
-
-    print(out)
